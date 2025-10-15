dee80469557fbdb3f07aacbb2d79ca3a47dbe63e
gitUser <caohaifeng92@163.com>
2025-10-15 17:26:31 +0800

    first commit

--- price.py
+++ price.py
@@ -6,7 +6,6 @@ from bs4 import BeautifulSoup
 import time
 import re
 import datetime
-import os
 
 # 直接获取实时金价
 gold_price = None
@@ -16,6 +15,8 @@ buy_weight = 0  # 默认值
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
 try:
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
